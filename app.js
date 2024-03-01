78f5f9c121ca1e14808908c0ea5c19a24ad13ef3
Mafoa <steve_mafoa@outlook.com>
2024-03-01 14:35:34 +0100

    Tableau des tâches

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+const tasks = [
+    {
+        title: "Apprendre à programmer",
+        done: false,
+    },
+    {
+        title: "Faire des courses",
+        done: true,
+    },
+]
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('todolist');
+    res.render('todolist', { tasks });
 });
 
 app.listen(port , () => {
